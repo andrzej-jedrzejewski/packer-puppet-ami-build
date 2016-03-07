50c2d4dc9b68f8a1a85db462ae63e03448531f35
Andrzej <a.jedrzejewski@kainos.com>
2016-03-07 16:09:12 +0100

    create resource tag for add visible image name in AWS

--- ubuntu/script.sh
+++ ubuntu/script.sh
@@ -48,6 +48,10 @@ done
 #Read image ID
 AMI_ID="$(aws ec2 describe-import-image-tasks --import-task-ids $import_task_id | jsawk  'return this.ImportImageTasks' | jsawk -n 'out(this.ImageId)')"
 
+sleep 5
+
+aws ec2 create-tags --resources ami-0307be70 --tags 'Key="Name",Value=$ova_file'
+
 echo "Image creation has finished!"
 echo "AMI ID is: $AMI_ID"
 
