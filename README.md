b8e085559df87b75170b0da510b494bf4694ca0b
Andrzej <a.jedrzejewski@kainos.com>
2016-03-07 13:45:24 +0100

    add sed to change S3import file

--- README.md
+++ README.md
@@ -10,8 +10,9 @@ AWS CLI:
 * Website: http://docs.aws.amazon.com/cli/latest/userguide/installing.html
 VM import specification:
 * Website http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/VMImportPrerequisites.html
+Create a bucket in S3
 
-
+aws ec2 describe-import-image-tasks --import-task-ids "import-ami-fh08peq4"
 
 Installation
 -----------
---
+++ ubuntu/script.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#This script allows us to:
+#1. generate VBO image withinh virtualbox to /tmp/output-
+#2. upload created image to amazon s3 bucket
+#3. verify created image
+#4. create test VM
+
+ova_directory="$(jsawk 'return this.output_directory' < variables.json)"
+ova_directory="$ova_directory$(jsawk 'return this.profile' < variables.json)"
+ova_file="$ova_file$(jsawk 'return this.os' < variables.json)"
+ova_file="$ova_file-$(jsawk 'return this.profile' < variables.json)"
+ova_file="$ova_file.ova"
+S3_bucket="$(jsawk 'return this.S3_bucket_name' < variables.json)"
+
+S3Key="$(cat S3import-task.json| jsawk 'return this.DiskContainers' | jsawk 'return this.UserBucket' | jsawk -n 'out(this.S3Key)')"
+sed -i -- "s/$S3Key/$ova_file/g" S3import-task.json
+
+
+start_date=$(date +%s);
+echo "You start at $start_date"
+
+if [ -a "$ova_directory/$ova_file" ]; then
+    rm -rf "$ova_directory"
+    echo "Old file has been deleted!"
+fi
+packer build -var-file=variables.json template.json  &&  echo "Image creation is done!" 
+
+aws s3 cp "$ova_directory/$ova_file" s3://$S3_bucket
+
+import_task_id="$(aws ec2 import-image --cli-input-json file://S3import-task.json | jsawk -n 'out(this.ImportTaskId)')"
+sleep 5s
+echo "Yor image ID is as follow:"
+echo $import_task_id
+echo "Your status of image creatio is as follow(update every minute):"
+
+import_task_progress="0"
+import_task_status="default"
+
+#Check in loop process progress
+while [ "$import_task_status" != "completed" ]; do
+    import_task_progress="$(aws ec2 describe-import-image-tasks --import-task-ids $import_task_id | jsawk  'return this.ImportImageTasks' | jsawk -n 'out(this.Progress)')"
+    import_task_status="$(aws ec2 describe-import-image-tasks --import-task-ids $import_task_id | jsawk  'return this.ImportImageTasks' | jsawk -n 'out(this.Status)')"
+    echo "Progress: $import_task_progress"
+    echo "Status: $import_task_status"
+    sleep 60
+done
+
+#Read image ID
+AMI_ID="$(aws ec2 describe-import-image-tasks --import-task-ids $import_task_id | jsawk  'return this.ImportImageTasks' | jsawk -n 'out(this.ImageId)')"
+
+echo "Image creation has finished!"
+echo "AMI ID is: $AMI_ID"
+
+#Calculate time
+stop_date=$(date +%s);
+echo "Whole process took:"
+echo $((stop_date-start_date)) | awk '{print int($1/60)":"int($1%60)}'
