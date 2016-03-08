eeb25ff244140b0a1f661ec2b95b6cb617091c8d
Andrzej <a.jedrzejewski@kainos.com>
2016-03-08 15:34:07 +0100

    add packer for jenkins

---
+++ jenkins/scripts/install-packer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installing Packer 0.8 .2 - unfortunately version is hardcoded right now"
+wget https://releases.hashicorp.com/packer/0.9.0/packer_0.9.0_linux_amd64.zip
+sudo unzip packer_0.9.0_linux_amd64.zip -d /usr/local/bin/
+
+
