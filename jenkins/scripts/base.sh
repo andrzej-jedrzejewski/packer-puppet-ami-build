761a4b7b312ab3b4c1e9987d9a69c70e8da73217
Andrzej <a.jedrzejewski@kainos.com>
2016-03-09 11:56:04 +0100

    change ubuntu home directory owner

--- jenkins/scripts/base.sh
+++ jenkins/scripts/base.sh
@@ -7,3 +7,7 @@ sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
+
+mkdir -p /home/ubuntu/.ssh
+chown -R ubuntu:ubuntu /home/ubuntu
+
